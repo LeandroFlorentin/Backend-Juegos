44dd534304595264691e72eaf3a2cf19a50a3db5
unknown <leandro.florentin@hotmail.com>
2022-12-07 20:56:47 -0300

    volvio

--- src/controllers/videogame.js
+++ src/controllers/videogame.js
@@ -2,34 +2,33 @@ const { Videogame, Genero } = require('../db.js')
 const { traerJuegos, traerJuego } = require('../helper')
 
 const mostrarTodo = async (req, res, next) => {
-    res.status(200).json("hola")
-    /*     const { name } = req.query
-        try {
-            let juegosApi = await traerJuegos();
-            let juegos = await Videogame.findAll({
-                include: [{ model: Genero, attributes: ['nombre'], through: { attributes: [] } }]
-            })
-            let totalJuegos = juegos.concat(juegosApi)
-            if (!name) {
-                res.status(200).json(totalJuegos)
-            } else {
-                let arrayEnviar = []
-                let i = 0;
-                while (true) {
-                    if (arrayEnviar.length === 15) break
-                    if (i === totalJuegos.length - 1) break
-                    let arrayName = totalJuegos[i].name.split(" ").join("").toLowerCase()
-                    let busqueda = name.split(" ").join("").toLowerCase()
-                    if (arrayName.includes(busqueda)) {
-                        arrayEnviar.push(totalJuegos[i])
-                    }
-                    i++
+    const { name } = req.query
+    try {
+        let juegosApi = await traerJuegos();
+        let juegos = await Videogame.findAll({
+            include: [{ model: Genero, attributes: ['nombre'], through: { attributes: [] } }]
+        })
+        let totalJuegos = juegos.concat(juegosApi)
+        if (!name) {
+            res.status(200).json(totalJuegos)
+        } else {
+            let arrayEnviar = []
+            let i = 0;
+            while (true) {
+                if (arrayEnviar.length === 15) break
+                if (i === totalJuegos.length - 1) break
+                let arrayName = totalJuegos[i].name.split(" ").join("").toLowerCase()
+                let busqueda = name.split(" ").join("").toLowerCase()
+                if (arrayName.includes(busqueda)) {
+                    arrayEnviar.push(totalJuegos[i])
                 }
-                res.status(200).json(arrayEnviar)
+                i++
             }
-        } catch (error) {
-            next(error)
-        } */
+            res.status(200).json(arrayEnviar)
+        }
+    } catch (error) {
+        next(error)
+    }
 }
 
 const mostrarUno = async (req, res, next) => {
