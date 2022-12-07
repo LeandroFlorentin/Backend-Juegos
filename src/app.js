bd0b277c74897ec2d08b65482addc3751ccb9418
unknown <leandro.florentin@hotmail.com>
2022-12-07 19:55:35 -0300

    cosas

--- src/app.js
+++ src/app.js
@@ -21,12 +21,10 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  /*   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE'); */
   next();
 });
-server.get('/', (req, res) => {
-  res.status(200).json("la pagina cargo bien")
-})
+
 server.use('/', routes);
 
 // Error catching endware.
