12011a67c5557f4bb4e129407d02438d461d984a
unknown <leandro.florentin@hotmail.com>
2022-12-07 18:07:12 -0300

    pg3

--- src/controllers/loadDB.js
+++ src/controllers/loadDB.js
@@ -1,5 +1,6 @@
 const { traerGeneros } = require('../helper')
 const { Genero } = require('../db.js')
+require('pg')
 
 module.exports = async () => {
     let generos = await traerGeneros()
--- src/helper/index.js
+++ src/helper/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+require('pg')
 
 const apiKey = "9f66ff818d524f568275bc55ca2257c4";
 
