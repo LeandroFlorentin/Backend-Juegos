191689c3f34840298445de74e6b21f845b94d077
unknown <leandro.florentin@hotmail.com>
2022-12-07 15:25:06 -0300

    cambios

--- index.js
+++ index.js
@@ -22,12 +22,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const loadDB = require('./src/controllers/loadDB.js')
 
-const PORT = process.env.PORT || 3001;
-
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
   loadDB()
-  server.listen(PORT, () => {
+  server.listen(process.env.PORT, () => {
     console.log("FUNCIONANDO EN " + PORT);
   });
 }); 
