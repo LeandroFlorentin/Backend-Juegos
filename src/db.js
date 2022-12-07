e04429928c5a5362dab5c2db533b504996b2b6bf
unknown <leandro.florentin@hotmail.com>
2022-12-06 22:04:22 -0300

    cambioDePuerto

--- src/db.js
+++ src/db.js
@@ -5,9 +5,9 @@ const path = require('path');
 const {
   DB_DEPLOY
 } = process.env;
-const sequelize = new Sequelize('postgresql://postgres:0YBAj1fJS0SAHwsw90B2@containers-us-west-117.railway.app:7089/railway', {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize("postgresql://postgres:0YBAj1fJS0SAHwsw90B2@containers-us-west-117.railway.app:7089/railway", {
+  logging: false,
+  native: false,
 });
 
 const basename = path.basename(__filename);
