ebe3affb7d59a7fd61a8d68f409f5ddf1a0d9817
Leandro Florentin <102199433+LeandroFlorentin@users.noreply.github.com>
2022-12-25 10:32:30 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -10,6 +10,11 @@ const {
 const sequelize = new Sequelize("postgres://db_videogames_user:nCpRwTXMd2G8Fkd8mVpLGtMLa6WGpyG9@dpg-ce8cge02i3mlmrfb55hg-a.oregon-postgres.render.com/db_videogames", {
   logging: false,
   native: false,
+  dialectOptions:{
+    ssl:{
+      require:true,
+    }
+  }
 });
 
 const basename = path.basename(__filename);
